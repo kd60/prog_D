45acb1af618d6cce5c02882ad105e2b91353c236
kd60 <93713155+kd60@users.noreply.github.com>
2021-11-24 15:28:52 +0300

    Update run.py

--- project2/app/run.py
+++ project2/app/run.py
@@ -2,9 +2,9 @@ import json
 import plotly
 import pandas as pd
 
-
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+
 
 from flask import Flask
 from flask import render_template, request, jsonify
