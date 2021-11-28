1a65d86634bf5517ed08ad9e4ae50de5cc211700
kd60 <93713155+kd60@users.noreply.github.com>
2021-11-28 12:46:42 +0300

    Update run.py

--- project2/app/run.py
+++ project2/app/run.py
@@ -6,13 +6,13 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 
-
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 import joblib
 
 from sqlalchemy import create_engine
+
 
 
 app = Flask(__name__)
